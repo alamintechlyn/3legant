ee544eb705e43b3f9d91991400f27f3409dfca57
alamin <alamintechlyn>
2025-05-11 09:40:00 +0600

    blog image add

--- src/compnents/blog/BlogList.jsx
+++ src/compnents/blog/BlogList.jsx
@@ -6,63 +6,63 @@ const BlogList = () => {
     {
       id: 1,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/pexels-kindelmedia-6573709.jpg?v=1746933627",
       title: "7 ways to decor your home like a professional",
       date: "October 16, 2023",
     },
     {
       id: 2,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/9ab0de662e28fa10e659a1acbbc99ecb89d85e7a.jpg?v=1746112228",
       title: "Inside a beautiful kitchen organization",
       date: "October 16, 2023",
     },
     {
       id: 3,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/7b5fe67e42604bcc5d365e977b486682beeb66de.jpg?v=1746111324",
       title: "Decor your bedroom for your children",
       date: "October 16, 2023",
     },
     {
       id: 4,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/58240a3d128558f36df46b415a06f2ff2df59df6.jpg?v=1746111324",
       title: "Modern texas home is beautiful and completely kid-friendly",
       date: "October 16, 2023",
     },
     {
       id: 5,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/6083781f02748b31636ad6f6111f0c2c7bac4c21.jpg?v=1746112215",
       title: "Modern texas home is beautiful and completely kid-friendly",
       date: "October 16, 2023",
     },
     {
       id: 6,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/fc6aee3082f8080b7bc0c5245a7472d96c9e2490.jpg?v=1746112225",
       title: "Modern texas home is beautiful and completely kid-friendly",
       date: "October 16, 2023",
     },
     {
       id: 7,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/7b5fe67e42604bcc5d365e977b486682beeb66de.jpg?v=1746111324",
       title: "Modern texas home is beautiful and completely kid-friendly",
       date: "October 16, 2023",
     },
     {
       id: 8,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/pexels-kindelmedia-6573695.jpg?v=1746933631",
       title: "Modern texas home is beautiful and completely kid-friendly",
       date: "October 16, 2023",
     },
     {
       id: 9,
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpI8UuteJCH3A4yaxSZdnazDgXacl2qAWpw&s",
+        "https://cdn.shopify.com/s/files/1/0639/7352/3521/files/c456d04e4963ce2362c0dd6120739fdb60471759.jpg?v=1746112222",
       title: "Modern texas home is beautiful and completely kid-friendly",
       date: "October 16, 2023",
     },
